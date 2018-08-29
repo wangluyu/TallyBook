8ed264c65cbeb8f18873ca9eb9a7c684f869eb56
wangluyu <13728742101@163.com>
2018-08-29 23:55:41 +0800

    update

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -9,6 +9,6 @@ class TestController extends Controller
 {
     public function test(Request $request)
     {
-        var_dump($request);
+        echo "aa";
     }
 }
--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\User;
 use App\Http\Requests\Api\UserRequest;
+use Illuminate\Support\Facades\DB;
 
 
 class UsersController extends Controller
