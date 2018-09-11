6deee0194e00c5f22ca268fc6bf388fd660bbe58
wangluyu <13728742101@163.com>
2018-09-11 17:43:28 +0800

    fix bug

--- database/migrations/2018_08_31_142734_create_partner_books_table.php
+++ database/migrations/2018_08_31_142734_create_partner_books_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePartnerBookTable extends Migration
+class CreatePartnerBooksTable extends Migration
 {
     /**
      * Run the migrations.
