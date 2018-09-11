9047a4a5f5831bcae3ba18976083606eb69d0459
wangluyu <13728742101@163.com>
2018-09-11 17:42:05 +0800

    fix bug

--- database/migrations/2018_08_29_112400_create_user_books_table.php
+++ database/migrations/2018_08_29_112400_create_user_books_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserBookTable extends Migration
+class CreateUserBooksTable extends Migration
 {
     /**
      * Run the migrations.
